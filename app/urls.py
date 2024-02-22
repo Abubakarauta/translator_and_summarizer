fa402f32d6380430170b85b9717a46fd93b8e36d
Abubakarauta <abubakarkabirauta@gmail.com>
2024-02-22 08:02:24 +0100

    modified temp

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,14 @@ urlpatterns = [
     path('', index, name='index'),
     path('summarizer', summarizer, name='summarizer'),
     path('translator', translator, name='translator'),
-    path('users-profile', users_profile, name='users-profile'),
+    path('usersProfile', usersProfile, name='usersProfile'),
+    path('faq', faq , name='faq'),
+    path('contact', contact, name='contact'),
+    path('register', register, name='register'),
+    path('login', login, name='login'),
+    path('error', error, name='error'),
+    path('blank', blank, name='blank'),
+
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -10,6 +10,26 @@ def summarizer(request):
 def translator(request):
     return render(request, 'translator.html')   
 
-def users_profile(request):
-    return render(render, 'users-profile.html')
+def usersProfile(request):
+    return render(request, 'usersProfile.html')
 
+def faq(request):
+    return render(request, 'pages-faq.html')
+
+def contact(request):
+    return render(request, 'pages-contact.html')
+
+def register(request):
+    return render(request, 'pages-register.html')
+
+
+def login(request):
+    return render(request, 'pages-login.html')
+
+
+def error(request):
+    return render(request, 'pages-error-404.html')
+
+
+def blank(request):
+    return render(request, 'pages-blank.html')
